b6f6be86475b9992031a5e249f24cf745706f884
Said Maadan <saidmaadan@gmail.com>
2024-05-29 01:52:34 -0500

    Added favico

--- theresa_eustace/settings.py
+++ theresa_eustace/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = ["www.theresaeustace.com",
                  "http://www.theresaeustace.com", "https://www.theresaeustace.com", "theresaeustace.com", "https://theresa-eustace.up.railway.app", "0.0.0.0", "localhost", "*"]
 
